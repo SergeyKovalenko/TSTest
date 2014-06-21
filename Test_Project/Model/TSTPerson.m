32fbe2302802d3e40353647324ffd2d7c9713bb3
Yury Grinenko <grinenko.y@gmail.com>
2014-06-21 14:58:52 +0300

    moved to nsmutabledictionary model

--- Test_Project/Model/TSTPerson.m
+++ Test_Project/Model/TSTPerson.m
@@ -8,7 +8,55 @@
 
 #import "TSTPerson.h"
 
+static NSString *const kFirstNameKey = @"firstName";
+static NSString *const kLastNameKey = @"lastName";
+static NSString *const kBirthDateKey = @"birthDate";
+
+@interface TSTPerson ()
+
+@property (nonatomic, strong) NSMutableDictionary *modelDictionary;
+
+@end
+
 @implementation TSTPerson
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _modelDictionary = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (NSString *)firstName {
+//    return _firstName;
+    return [self.modelDictionary objectForKey:kFirstNameKey];
+}
+
+- (void)setFirstName:(NSString *)firstName {
+//    _firstName = firstName;
+    [self.modelDictionary setObject:firstName forKey:kFirstNameKey];
+}
+
+- (NSString *)lastName {
+//    return _lastName;
+    return [self.modelDictionary objectForKey:kLastNameKey];
+}
+
+- (void)setLastName:(NSString *)lastName {
+//    _lastName = lastName;
+    [self.modelDictionary setObject:lastName forKey:kLastNameKey];
+}
+
+- (NSDate *)birthDate {
+//    return _birthDate;
+    return [self.modelDictionary objectForKey:kBirthDateKey];
+}
+
+- (void)setBirthDate:(NSDate *)birthDate {
+//    _birthDate = birthDate;
+    [self.modelDictionary setObject:birthDate forKey:kBirthDateKey];
+}
 
 - (NSString *)description
 {
@@ -18,3 +66,8 @@
 }
 
 @end
+
+
+
+
+
